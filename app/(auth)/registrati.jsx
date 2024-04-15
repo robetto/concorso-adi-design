6daebc8d2c758f18d59b2ca910e1d1ff5b44ece4
robetto <robertobettoni@gmail.com>
2024-04-15 17:12:09 +0200

    daje committa però

---
+++ app/(auth)/registrati.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Registrati = () => {
+  return (
+    <View>
+      <Text>Registrati</Text>
+    </View>
+  )
+}
+
+export default Registrati
